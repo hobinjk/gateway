40ba0ad85dd2d109c880b2273033c7b20b5c55cb
James Hobin <hobinjk@gmail.com>
2017-06-16 12:28:12 -0400

    Add mozilla-central's .eslintrc.js file

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+"use strict";
+
+module.exports = {
+  // New rules and configurations should generally be added in
+  // tools/lint/eslint/eslint-plugin-mozilla/lib/configs/recommended.js to
+  // allow external repositories that use the plugin to pick them up as well.
+  "extends": [
+    "plugin:mozilla/recommended"
+  ],
+  "plugins": [
+    "mozilla"
+  ]
+};
